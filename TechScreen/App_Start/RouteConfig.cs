d95c20a81dfeedd37ab1d09e543bb00878e70c25
yev <yev.berezovsky@gmail.com>
2018-06-16 10:40:08 -0400

    General Layout with Two Tabs

--- TechScreen/App_Start/RouteConfig.cs
+++ TechScreen/App_Start/RouteConfig.cs
@@ -16,8 +16,13 @@ namespace TechScreen
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "TechScreen", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                 name: "Hello",
+                 url: "{controller}/{action}/{name}/{id}"
+             );
         }
     }
 }
---
+++ TechScreen/Controllers/TechScreenController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TechScreen.Controllers
+{
+    public class TechScreenController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public ActionResult Welcome()
+        {   
+            return View();
+        }
+    }
+}
+
