b71df6a9c7bac541a300ef212278a2a5b92dd719
yev <yev.berezovsky@gmail.com>
2018-06-17 06:38:59 -0400

    FirstTab: formatting

--- TechScreen/Controllers/FirstTabController.cs
+++ TechScreen/Controllers/FirstTabController.cs
@@ -10,161 +10,161 @@ namespace TechScreen.Controllers
 {
     public class FirstTabController : ApiController
     {
-        public Employee [] Get()
+        public Employee[] Get()
         {
             return new Employee[]
 
-    {
-        new Employee {
-        Name = "Michael Bruce",
-        Position = "Javascript Developer",
-        Office = "Singapore",
-        Age = "29",
-        StartDate = "2011/06/27",
-        Salary = "$183,000"},
-        new Employee {
-        Name = "Tiger Nixon",
-        Position = "System Architect",
-        Office = "Edinburgh",
-        Age = "61",
-        StartDate = "2011/04/25",
-        Salary = "$320,800"},
-        new Employee {
-        Name = "Garrett Winters",
-        Position = "Accountant",
-        Office = "Tokyo",
-        Age = "63",
-        StartDate = "2011/07/25",
-        Salary = "$170,750"},
-        new Employee {
-        Name = "Cedric Kelly",
-        Position = "Senior Javascript Developer",
-        Office = "Edinburgh",
-        Age = "22",
-        StartDate = "2012/03/29",
-        Salary = "$433,060"},
-        new Employee {
-        Name = "Ashton Cox",
-        Position = "Junior Technical Author",
-        Office = "San Francisco",
-        Age = "66",
-        StartDate = "2009/01/12",
-        Salary = "$86,000"},
-        new Employee {
-        Name = "Airi Satou",
-        Position = "Javascript Developer",
-        Office = "Singapore",
-        Age = "29",
-        StartDate = "2011/06/27",
-        Salary = "$183,000"},
-        new Employee {
-        Name = "Herrod Chandler",
-        Position = "System Architect",
-        Office = "Edinburgh",
-        Age = "61",
-        StartDate = "2011/04/25",
-        Salary = "$320,800"},
-        new Employee {
-        Name = "Colleen Hurst",
-        Position = "Accountant",
-        Office = "Tokyo",
-        Age = "63",
-        StartDate = "2011/07/25",
-        Salary = "$170,750"},
-        new Employee {
-        Name = "Sonya Frost",
-        Position = "Senior Javascript Developer",
-        Office = "Edinburgh",
-        Age = "22",
-        StartDate = "2012/03/29",
-        Salary = "$433,060"},
-        new Employee {
-        Name = "Jena Gaines",
-        Position = "Junior Technical Author",
-        Office = "San Francisco",
-        Age = "66",
-        StartDate = "2009/01/12",
-        Salary = "$86,000"},
-        new Employee {
-        Name = "Joseph Roberts",
-        Position = "System Architect",
-        Office = "Edinburgh",
-        Age = "61",
-        StartDate = "2011/04/25",
-        Salary = "$320,800"},
-        new Employee {
-        Name = "Michael Bruce",
-        Position = "Javascript Developer",
-        Office = "Singapore",
-        Age = "29",
-        StartDate = "2011/06/27",
-        Salary = "$183,000"},
-        new Employee {
-        Name = "Tiger Nixon",
-        Position = "System Architect",
-        Office = "Edinburgh",
-        Age = "61",
-        StartDate = "2011/04/25",
-        Salary = "$320,800"},
-        new Employee {
-        Name = "Garrett Winters",
-        Position = "Accountant",
-        Office = "Tokyo",
-        Age = "63",
-        StartDate = "2011/07/25",
-        Salary = "$170,750"},
-        new Employee {
-        Name = "Cedric Kelly",
-        Position = "Senior Javascript Developer",
-        Office = "Edinburgh",
-        Age = "22",
-        StartDate = "2012/03/29",
-        Salary = "$433,060"},
-        new Employee {
-        Name = "Ashton Cox",
-        Position = "Junior Technical Author",
-        Office = "San Francisco",
-        Age = "66",
-        StartDate = "2009/01/12",
-        Salary = "$86,000"},
-        new Employee {
-        Name = "Airi Satou",
-        Position = "Javascript Developer",
-        Office = "Singapore",
-        Age = "29",
-        StartDate = "2011/06/27",
-        Salary = "$183,000"},
-        new Employee {
-        Name = "Herrod Chandler",
-        Position = "System Architect",
-        Office = "Edinburgh",
-        Age = "61",
-        StartDate = "2011/04/25",
-        Salary = "$320,800"},
-        new Employee {
-        Name = "Colleen Hurst",
-        Position = "Accountant",
-        Office = "Tokyo",
-        Age = "63",
-        StartDate = "2011/07/25",
-        Salary = "$170,750"},
-        new Employee {
-        Name = "Sonya Frost",
-        Position = "Senior Javascript Developer",
-        Office = "Edinburgh",
-        Age = "22",
-        StartDate = "2012/03/29",
-        Salary = "$433,060"},
-        new Employee {
-        Name = "Jena Gaines",
-        Position = "Junior Technical Author",
-        Office = "San Francisco",
-        Age = "66",
-        StartDate = "2009/01/12",
-        Salary = "$86,000"}
+            {
+                new Employee {
+                Name = "Michael Bruce",
+                Position = "Javascript Developer",
+                Office = "Singapore",
+                Age = "29",
+                StartDate = "2011/06/27",
+                Salary = "$183,000"},
+                new Employee {
+                Name = "Tiger Nixon",
+                Position = "System Architect",
+                Office = "Edinburgh",
+                Age = "61",
+                StartDate = "2011/04/25",
+                Salary = "$320,800"},
+                new Employee {
+                Name = "Garrett Winters",
+                Position = "Accountant",
+                Office = "Tokyo",
+                Age = "63",
+                StartDate = "2011/07/25",
+                Salary = "$170,750"},
+                new Employee {
+                Name = "Cedric Kelly",
+                Position = "Senior Javascript Developer",
+                Office = "Edinburgh",
+                Age = "22",
+                StartDate = "2012/03/29",
+                Salary = "$433,060"},
+                new Employee {
+                Name = "Ashton Cox",
+                Position = "Junior Technical Author",
+                Office = "San Francisco",
+                Age = "66",
+                StartDate = "2009/01/12",
+                Salary = "$86,000"},
+                new Employee {
+                Name = "Airi Satou",
+                Position = "Javascript Developer",
+                Office = "Singapore",
+                Age = "29",
+                StartDate = "2011/06/27",
+                Salary = "$183,000"},
+                new Employee {
+                Name = "Herrod Chandler",
+                Position = "System Architect",
+                Office = "Edinburgh",
+                Age = "61",
+                StartDate = "2011/04/25",
+                Salary = "$320,800"},
+                new Employee {
+                Name = "Colleen Hurst",
+                Position = "Accountant",
+                Office = "Tokyo",
+                Age = "63",
+                StartDate = "2011/07/25",
+                Salary = "$170,750"},
+                new Employee {
+                Name = "Sonya Frost",
+                Position = "Senior Javascript Developer",
+                Office = "Edinburgh",
+                Age = "22",
+                StartDate = "2012/03/29",
+                Salary = "$433,060"},
+                new Employee {
+                Name = "Jena Gaines",
+                Position = "Junior Technical Author",
+                Office = "San Francisco",
+                Age = "66",
+                StartDate = "2009/01/12",
+                Salary = "$86,000"},
+                new Employee {
+                Name = "Joseph Roberts",
+                Position = "System Architect",
+                Office = "Edinburgh",
+                Age = "61",
+                StartDate = "2011/04/25",
+                Salary = "$320,800"},
+                new Employee {
+                Name = "Michael Bruce",
+                Position = "Javascript Developer",
+                Office = "Singapore",
+                Age = "29",
+                StartDate = "2011/06/27",
+                Salary = "$183,000"},
+                new Employee {
+                Name = "Tiger Nixon",
+                Position = "System Architect",
+                Office = "Edinburgh",
+                Age = "61",
+                StartDate = "2011/04/25",
+                Salary = "$320,800"},
+                new Employee {
+                Name = "Garrett Winters",
+                Position = "Accountant",
+                Office = "Tokyo",
+                Age = "63",
+                StartDate = "2011/07/25",
+                Salary = "$170,750"},
+                new Employee {
+                Name = "Cedric Kelly",
+                Position = "Senior Javascript Developer",
+                Office = "Edinburgh",
+                Age = "22",
+                StartDate = "2012/03/29",
+                Salary = "$433,060"},
+                new Employee {
+                Name = "Ashton Cox",
+                Position = "Junior Technical Author",
+                Office = "San Francisco",
+                Age = "66",
+                StartDate = "2009/01/12",
+                Salary = "$86,000"},
+                new Employee {
+                Name = "Airi Satou",
+                Position = "Javascript Developer",
+                Office = "Singapore",
+                Age = "29",
+                StartDate = "2011/06/27",
+                Salary = "$183,000"},
+                new Employee {
+                Name = "Herrod Chandler",
+                Position = "System Architect",
+                Office = "Edinburgh",
+                Age = "61",
+                StartDate = "2011/04/25",
+                Salary = "$320,800"},
+                new Employee {
+                Name = "Colleen Hurst",
+                Position = "Accountant",
+                Office = "Tokyo",
+                Age = "63",
+                StartDate = "2011/07/25",
+                Salary = "$170,750"},
+                new Employee {
+                Name = "Sonya Frost",
+                Position = "Senior Javascript Developer",
+                Office = "Edinburgh",
+                Age = "22",
+                StartDate = "2012/03/29",
+                Salary = "$433,060"},
+                new Employee {
+                Name = "Jena Gaines",
+                Position = "Junior Technical Author",
+                Office = "San Francisco",
+                Age = "66",
+                StartDate = "2009/01/12",
+                Salary = "$86,000"}
 
 
-    }; 
+            };
         }
     }
 }
