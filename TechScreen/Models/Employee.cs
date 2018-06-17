fa5ed7224051e913be748b4b4c9fb91d7d947c6a
yev <yev.berezovsky@gmail.com>
2018-06-17 06:32:55 -0400

    FirstTab: Added web method to retrieve data and angularjs

---
+++ TechScreen/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechScreen.Models
+{
+    public class Employee
+    {
+        public string Name
+        { get; set; }
+        public string Position
+        { get; set; }
+        public string Office
+        { get; set; }
+        public string Age
+        { get; set; }
+        public string StartDate
+        { get; set; }
+        public string Salary
+        { get; set; }
+    }
+}
--- TechScreen/Scripts/TechScreenScripts/FirstTab.js
+++ TechScreen/Scripts/TechScreenScripts/FirstTab.js
@@ -1,5 +1,36 @@
-﻿$(document).ready(function () {
-    $('#example').DataTable();
-    $('#master_table').show();
-    
-});
+﻿angular.module('TechScreenApp', [])
+    .controller('FirstTabCtrl', function ($scope, $http) {
+        $scope.answered = false;
+        $scope.title = "loading question...";
+        $scope.options = [];
+        $scope.correctAnswer = false;
+        $scope.working = false;
+
+        $scope.answer = function () {
+            return $scope.correctAnswer ? 'correct' : 'incorrect';
+        };
+    });
+
+angular.module('showcase.angularWay', [])
+    .controller('AngularWayCtrl', AngularWayCtrl).directive('myPostRepeatDirective', function () {
+        return function ($scope, element, attrs) {
+            if ($scope.$last) {
+                $('#example').DataTable();
+                $('#master_table').show();
+            }
+        };
+    });
+
+function AngularWayCtrl($scope, $http) {
+    angular.element(document).ready(function () {
+    });
+    $scope.downloadData = function () {
+        $http.get("/api/FirstTab").then(function success(data, status, headers, config) {
+            $scope.persons = data.data;
+        });
+    }
+
+
+
+
+}
